c7114b20bfc005708777645ded9aede987c6aab5
achatman <andrew.chatman9@gmail.com>
2018-09-30 11:13:03 -0500

    Removed unused code.

--- output.cpp
+++ output.cpp
@@ -157,31 +157,6 @@ void histogram_fit_data(double fracs[6], indices_t ins, args_t *args, hists_t *h
   bkghist->Scale(bkg_int / dat_int);
   srchist->Scale(src_int / dat_int);
 
-  //Draw Std Src Fit
-  /*
-  bkghist->Scale(fracs[0]);
-  srchist->Scale(fracs[1]);
-  src_noBB(fracs[0], fracs[1], false, F0);
-  c1->cd(1);
-  F0->Draw("hist");
-  dathist->Draw("same");
-  bkghist->Draw("same hist");
-  srchist->Draw("same hist");
-
-  F0->SetMinimum(0);
-  F0->SetMaximum(std::max(
-    std::max(dathist->GetMaximum(), F0->GetMaximum()),
-                          std::max(srchist->GetMaximum(), bkghist->GetMaximum())
-  ) * 1.1);
-
-  legend0->AddEntry(F0, "fi");
-  legend0->AddEntry(bkghist, "bi");
-  legend0->AddEntry(dathist, "di");
-  legend0->AddEntry(srchist, "Si");
-  legend0->Draw();
-  srchist->Scale(1/fracs[1]);
-  */
-
   //Draw BB Src
   srchist->Scale(fracs[3]);
   src_BB(fracs[2], fracs[3], false, F1, B1);
@@ -215,17 +190,6 @@ void histogram_fit_data(double fracs[6], indices_t ins, args_t *args, hists_t *h
   c1->cd(4);
   TPaveText *pt = new TPaveText(0, 0, 1, 1);
   std::stringstream line;
-  /*
-  pt->AddText(.05, .95, "Standard:")->SetTextAlign(12);
-  line << "P_b = " << fracs[0];
-  pt->AddText(.05, .85, line.str().c_str())->SetTextAlign(12);
-  line.str("");
-  line << "P_s = " << fracs[1];
-  pt->AddText(.05, .8, line.str().c_str())->SetTextAlign(12);
-  line.str("");
-  line << "TS = " << -2 * (src_noBB(fracs[0], fracs[1]) - nosrc_noBB(fracs[4]));
-  pt->AddText(.05, .75, line.str().c_str())->SetTextAlign(12);
-  */
   pt->AddText(.35, .95, "Barlow-Beeston:")->SetTextAlign(12);
   line.str("");
   line << "P_b = " << fracs[2];
@@ -332,53 +296,6 @@ void print_errors(TFitter *fitter, std::string fit_param, std::string pathbase){
     << PRINTSPACE << eminus
     << PRINTSPACE << eparab
     << PRINTSPACE << globcc << std::endl;
-  f.close();
-}
-
-
-void calculate_errors(double Pb_m, double Ps_m, double sigma_Pb_m, double sigma_Ps_m, indices_t ins, double alpha, hists_t hists){
-  double N_D = hists.msw_dat->Integral();
-  double N_B = hists.msw_bkg->Integral();
-  double B = alpha * N_B;
-  double S = N_D - B;
-  double P_S = S / N_D;
-  double P_B = B / N_D;
-
-  double sigma_N_D = TMath::Sqrt(hists.msw_dat->Integral());
-  double sigma_B = TMath::Sqrt(alpha) * TMath::Sqrt(B);
-  double sigma_S = TMath::Sqrt( TMath::Power(sigma_N_D, 2) + TMath::Power(sigma_B, 2) );
-  double sigma_P_S = TMath::Abs(P_S) * TMath::Sqrt( TMath::Power(sigma_S / S, 2) + TMath::Power(sigma_N_D / N_D, 2) );
-  double sigma_P_B = TMath::Abs(P_B) * TMath::Sqrt( TMath::Power(sigma_B / B, 2) + TMath::Power(sigma_N_D / N_D, 2) );
-
-  std::stringstream filename;
-  filename << "Errors_" << hists.outpath << ".csv";
-  std::ofstream f(filename.str().c_str());
-  f << "Minuit Values:" << std::endl
-  << "Ps: " << Ps_m << std::endl
-  << "sigma_Ps: " << sigma_Ps_m << std::endl
-  << "Pb: " << Pb_m << std::endl
-  << "sigma_Pb: " << sigma_Pb_m << std::endl;
-  f << std::endl;
-  f << "Calculated Values:" << std::endl
-  << "Ps: " << P_S << std::endl
-  << "sigma_Ps: " << sigma_P_S << std::endl
-  << "Pb: " << P_B << std::endl
-  << "sigma_Pb: " << sigma_P_B << std::endl;
-  f << std::endl;
-  f << "Relative Differences:" << std::endl
-  << "Ps: " << TMath::Abs(P_S - Ps_m) / Ps_m << std::endl
-  << "sigma_Ps: " << TMath::Abs(sigma_Ps_m - sigma_P_S) / sigma_Ps_m << std::endl
-  << "Pb: " << TMath::Abs(P_B - Pb_m) / Pb_m << std::endl
-  << "sigma_Pb: " << TMath::Abs(sigma_Pb_m - sigma_P_B) / sigma_Pb_m << std::endl;
-  f << std::endl;
-  f << "N_D: " << N_D << std::endl
-  << "sigma_N_D: " << sigma_N_D << std::endl
-  << "B: " << B << std::endl
-  << "sigma_B: " << sigma_B << std::endl
-  << "S: " << S << std::endl
-  << "sigma_S: " << sigma_S << std::endl
-  << "alpha: " << alpha << std::endl;
-
   f.close();
 }
 
